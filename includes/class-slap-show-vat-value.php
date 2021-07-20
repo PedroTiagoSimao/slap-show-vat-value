42bd62735032a7eb1ba603e5a69830c181ef29a8
Pedro Simao <pedrosimao@gmail.com>
2021-07-20 15:35:38 +0100

    Added logic to show VAT value and margin

---
+++ includes/class-slap-show-vat-value.php
@@ -0,0 +1,34 @@
+<?php
+
+class SLAP_VAT_Value {
+
+    public function run() {
+
+        add_filter('woocommerce_get_price_html', 'edit_price_display', 10, 2);
+
+        function edit_price_display () {
+            global $product;
+            $price = $product->price;
+            
+            $tax_rates = WC_Tax::get_rates( $product->get_tax_class() );
+            if(!empty($tax_rates)) {
+                $tax_rate = reset($tax_rates);
+                $tax_cal = ($tax_rate['rate']/100)+1;
+                
+                $tax = number_format($price-($price/$tax_cal),2,',','');  
+                echo '
+                    <span class="price">
+                        <span class="woocommerce-Price-amount amount">
+                            <bdi>
+                                <span class="woocommerce-Price-currencySymbol">€</span>
+                                ' . $price . '  <span class="single-product-vat">
+                                                    (inclui ' . $tax . '€ IVA '.$tax_rate['rate'].'%)
+                                                </span>
+                            </bdi>
+                        </span>
+                    </span>
+                ';
+            }
+        }
+    }
+}
---
+++ slap-show-vat-value.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link              https://slap.pt/
+ * @since             1.0.1
+ * @package           Vat_Value
+ *
+ * Plugin Name: SLAP - Mostrar IVA para WooCommerce
+ * Plugin URI: https://slap.pt/
+ * Description: SLAP - Mostrar IVA para WooCommerce
+ * Author: SLAP
+ * Author URI: https://slap.pt/
+ * Version: 1.0.1
+ */
+
+require_once (dirname(__FILE__) . '/includes/class-slap-show-vat-value.php');
+
+$slap_vat_value = new SLAP_VAT_Value;
+$slap_vat_value->run();
